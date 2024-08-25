3f71fc1b96aeda0eeb8b2cb0b459ece228c3aaf5
willcking <19372142252@163.com>
2024-08-26 01:00:31 +0800

    Add your commit message here

--- contracts/redpacket/RedPacket.sol
+++ contracts/redpacket/RedPacket.sol
@@ -35,13 +35,26 @@ contract RedPacket is Initializable {
         uint duration
     );
 
+    event ClaimSuccess(
+        bytes32 id,
+        address claimer,
+        uint claimed_value,
+        address token_address
+    );
+
+    event RefundSuccess(
+        bytes32 id,
+        address token_address,
+        uint remaining_balance
+    );
+
     using SafeERC20 for IERC20;
     uint32 nonce;
     bytes32 private seed;
     mapping(bytes32 => RedPackets) redpacket_by_id;
 
-    constructor() public initializer {
-        seed = keccak256(abi.encodePacked("RedPacket", block.timestamp, msg.sender));
+    function initialize() public initializer {
+        seed = keccak256(abi.encodePacked(" Redpacket ", block.timestamp, msg.sender));
     }
 
     function create_redPacked
@@ -100,13 +113,113 @@ contract RedPacket is Initializable {
     function claim(bytes32 id, bytes32[] memory proof) public returns(uint claimed) {
         RedPackets storage redp = redpacket_by_id[id];
         Packed memory packed = redp.packed;
-
         require(unbox(packed.packed1, 224, 32) > block.timestamp, "Expired");
 
+        uint recipient_number = unbox(packed.packed2, 239, 15);
+        uint claimed_number = unbox(packed.packed2, 224, 15);
+        require (claimed_number < recipient_number, "Out of stock");
 
+        bytes32 merkleroot = redp.merkleRoot;
+        require(MerkleProof.verify(proof, merkleroot, _leaf(msg.sender)), 'Verification failed');
+
+        uint256 claimed_tokens;
+        uint256 token_type = unbox(packed.packed2, 254, 1);
+        uint256 ifrandom = unbox(packed.packed2, 255, 1);
+        uint256 remaining_tokens = unbox(packed.packed1, 128, 96);
+        address token_address = address(uint160(unbox(packed.packed2, 64, 160)));
+        uint minium_value = 10**(IERC20(token_address).decimals() - 1);
+
+        if(ifrandom == 1) {
+            if (recipient_number - claimed_number == 1) {
+                claimed_tokens = remaining_tokens;
+            } else {
+                uint reserve_amount = (recipient_number - claimed_number) * minium_value;
+                uint distribute_tokens = remaining_tokens - reserve_amount;
+                claimed_tokens = random(seed, nonce) % (distribute_tokens * 2/ (recipient_number - claimed_number));
+                claimed_tokens = claimed_tokens < minium_value ? minium_value : (claimed_tokens - (claimed_tokens % minium_value));
+            }
+        } else {
+            if (recipient_number - claimed_number == 1) {
+                claimed_tokens = remaining_tokens;
+            } else {
+                claimed_tokens = remaining_tokens/(recipient_number - claimed_number);
+            }
+        }
+
+        redp.packed.packed1 = rewriteBox(packed.packed1, 128, 96, remaining_tokens - claimed_tokens);
+
+        require(redp.claim_list[msg.sender] == 0, "Already claimed");
+
+        redp.claim_list[msg.sender] = claimed_tokens;
+        redp.packed.packed2 = rewriteBox(packed.packed2, 224, 15, claimed_number + 1);
+
+        if (token_type == 0) {
+            payable(msg.sender).transfer(claimed_tokens);
+        } else if (token_type == 1) {
+            transfer_token(token_address, msg.sender, claimed_tokens);
+        }
+        emit ClaimSuccess(id, msg.sender, claimed_tokens, token_address);
+        return claimed_tokens;
+    }
+
+    function refund(bytes32 id) public {
+        RedPackets storage redp = redpacket_by_id[id];
+        Packed memory packed = redp.packed;
+        address owner = redp.owner;
+        require(owner == msg.sender, "Redpacked Owner Only");
+        require(unbox(packed.packed1, 224, 32) <= block.timestamp, "Not expired yet");
+        uint256 remaining_tokens = unbox(packed.packed1, 128, 96);
+        require(remaining_tokens != 0, "None left in the red packet");
+
+        uint256 token_type = unbox(packed.packed2, 254, 1);
+        address token_address = address(uint160(unbox(packed.packed2, 64, 160)));
+
+        redp.packed.packed1 = rewriteBox(packed.packed1, 128, 96, 0);
+
+        if (token_type == 0) {
+            payable(msg.sender).transfer(remaining_tokens);
+        }
+        else if (token_type == 1) {
+            transfer_token(token_address, msg.sender, remaining_tokens);
+        }
+
+        emit RefundSuccess(id, token_address, remaining_tokens);
+    }
+
+    function getRedpacked(bytes32 id) external view returns 
+    (
+        address token_address,
+        uint balance, 
+        uint total, 
+        uint claimed, 
+        bool expired, 
+        uint256 claimed_amount
+    ) {
+        RedPackets storage redp = redpacket_by_id[id];
+        Packed memory packed = redp.packed;
+        return (
+            address(uint160(unbox(packed.packed2, 64, 160))), 
+            unbox(packed.packed1, 128, 96), 
+            unbox(packed.packed2, 239, 15), 
+            unbox(packed.packed2, 224, 15), 
+            block.timestamp > unbox(packed.packed1, 224, 32), 
+            redp.claim_list[msg.sender]
+        );
     }
 
 //-----------------------------Calculation tools-------------------------------------
+    function transfer_token(address token_address, address recipient_address, uint amount) internal{
+        IERC20(token_address).safeTransfer(recipient_address, amount);
+    }
+
+    function random(bytes32 _seed, uint32 nonce_rand) internal view returns (uint rand) {
+        return uint(keccak256(abi.encodePacked(nonce_rand, msg.sender, _seed, block.timestamp))) + 1 ;
+    }
+
+    function _leaf(address account) internal pure returns (bytes32){
+        return keccak256(abi.encodePacked(account));
+    }
+
     function box (uint16 position, uint16 size, uint256 data) internal pure returns (uint256 boxed) {
         require(validRange(size, data), "Value out of range BOX");
         assembly {
@@ -127,6 +240,14 @@ contract RedPacket is Initializable {
         assembly {
             // 2^size > data or size ==256
             ifValid := or(eq(size, 256), gt(shl(size, 1), data))
+        }
+    }
+
+    function rewriteBox (uint256 _box, uint16 position, uint16 size, uint256 data) internal pure returns (uint256 boxed) {
+        assembly {
+            // mask = ~((1 << size - 1) << position)
+            // _box = (mask & _box) | ()data << position)
+            boxed := or( and(_box, not(shl(position, sub(shl(size, 1), 1)))), shl(position, data))
         }
     }
 
